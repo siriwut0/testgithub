f1c1b8fe70e11b5161928d2fb1230e154c2ad4da
siriwut0 <siriwut.e@gmail.com>
2019-12-29 11:18:26 +0700

    testpro

---
+++ FirstPro/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstPro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculate_Click(object sender, RoutedEventArgs e)
+        {
+            float minas = float.Parse(income.Text) - float.Parse(paid.Text);
+            float div = float.Parse(value.Text) / minas;
+            howManyDay.Text = div.ToString();
+        }
+    }
+}
